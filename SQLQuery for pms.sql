e8d2aa0b5157f45c387a4ad446b89303e8f248cf
Nadirrashidi <111681837+Nadirrashidi@users.noreply.github.com>
2024-06-20 10:53:17 +0530

    Add files via upload

---
+++ SQLQuery for pms.sql
@@ -0,0 +1,45 @@
+select * from Employees
+select * from Users
+
+insert into Employees (name, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir' , 'siwan' , 'india' , '0' , 'aaaa', 1-1-2000, 'bbb' ,2-2-200)
+
+select * from Employees
+
+insert into Employees (name, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir1' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
+insert into Employees (name, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir2' , 'siwan2' , 'india2' , '1' , 'aaaa2', 1-1-2002, 'bbb2' ,2-2-2002)
+insert into Employees (name, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir3' , 'siwan3' , 'india3' , '1' , 'aaaa2', 1-1-2003, 'bbb' ,2-2-2003)
+insert into Employees (name, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir4' , 'siwan4' , 'india4' , '0' , 'aaaa2', 1-1-2004, 'bbb' ,2-2-2004)
+
+select * from Users
+
+insert into Users(fname,lname, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir1', 'rashidi' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
+
+
+insert into Users(fname,lname, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir2', 'rashidi1' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
+
+
+insert into Users(fname,lname, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir3', 'rashidi2' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
+
+
+insert into Users(fname,lname, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir4', 'rashidi3' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
+
+
+insert into Users(fname,lname, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir5', 'rashidi4' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
+
+
+insert into Users(fname,lname, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir6', 'rashidi5' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
+
+
+insert into Users(fname,lname, city , country ,IsDelete , CreatedBy ,CreatedDate ,UpdatedBy ,UpdatedDate)
+values ('nadir1', 'rashidi6' , 'siwan1' , 'india1' , '0' , 'aaaa1', 1-1-2001, 'bbb1' ,2-2-201)
---
+++ SQLQuery2.sql
@@ -0,0 +1,13 @@
+create table userpmss(
+fname varchar(20),
+lname varchar(20),
+city varchar (20),
+country varchar(20)
+);
+
+insert into userpmss(fname,lname,city,country) values 
+('Nadir' , 'Rashidi' ,'new delhi' , 'india');
+
+insert into userpmss(fname,lname,city,country) values 
+('rashid' , 'jilani' ,'gujrat' , 'india');
+drop table 
